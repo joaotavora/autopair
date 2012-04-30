81568b51eb29e628fb5d76a935887ed27996d19e
Joao Tavora <joaotavora@gmail.com>
2012-04-30 18:41:54 +0100

    `autopair-tests` now written in ert

--- autopair-tests.el
+++ autopair-tests.el
@@ -20,202 +20,136 @@
 
 ;;; Commentary:
 
-;; Mini test framework for autopair.el
+;; Tests for autopair.el
 
 ;;; Code:
 (require 'autopair)
-(setq autopair-extra-tests (list (list "       "
-                                       "-----`-"
-                                       #'autopair-extra-pair-p
-                                       "-----y-"
-                                       '((autopair-extra-pairs '(:everywhere ((?` . ?'))))))
-                                 (list "\"     \""
-                                       "-----`-"
-                                       #'autopair-extra-pair-p
-                                       "-----y-"
-                                       '((autopair-extra-pairs '(:string ((?` . ?'))))))
-                                 (list "   ` ' "
-                                       "-----'-"
-                                       #'autopair-extra-skip-p
-                                       "-----y-"
-                                       '((autopair-extra-pairs '(:everywhere ((?` . ?'))))))
-                                 (list "  \"   \""
-                                       "-`---`-"
-                                       #'autopair-extra-pair-p
-                                       "-----y-"
-                                       '((autopair-extra-pairs '(:string ((?` . ?'))))))))
+(require 'ert)
+(require 'ert-x)
 
 
+(defmacro define-autopair-simple-predicate-test (name fixture input predicate expectation &optional bindings)
+  (declare (indent defun))
+  `(ert-deftest ,(intern (concat "autopair-simple-predicate-test-" (symbol-name name))) ()
+     ,(format "With \"%s\", call `%s' for \"%s\". Should get \"%s\""
+              fixture
+              (symbol-name predicate)
+              input
+              expectation)
+     (with-temp-buffer
+       (let ,bindings
+         (autopair-mode 1)
+         (insert ,fixture)
+         (let* ((size (1- (point-max)))
+                (result (make-string size ?-)))
+           (dotimes (i size)
+             (goto-char (1+ i))
+             (let ((autopair-inserted (aref ,input i)))
+               (when (and (not (eq autopair-inserted ?-))
+                          (funcall #',predicate)
+                          (aset result i ?y)))))
+           (should (string= result ,expectation)))))))
 
-;; mini test-framework for the decision making predicates
+(defmacro define-autopair-functional-test (name fixture-fn input expected-text expected-point &optional bindings)
+  (declare (indent defun))
+  `(ert-deftest ,(intern (concat "autopair-functional-test-" (symbol-name name))) ()
+     ,(format "%s: see test definition" name)
+     (with-temp-buffer
+       (let ,bindings
+         (autopair-mode 1)
+         (funcall ,fixture-fn)
+         (cond ((and (symbolp ,input)
+                     (commandp ,input))
+                (ert-simulate-command ,input))
+               ((stringp ,input)
+                (let ((last-command-event (aref ,input 0)))
+                  (call-interactively (key-binding ,input) nil)
+                  (autopair-post-command-handler))))
+         (should (string= (buffer-substring-no-properties (point-min) (point-max)) ,expected-text))
+         (should (eql (point) ,expected-point))))))
+
+;; basic tests
 ;;
-(defvar autopair-tests)
-(setq autopair-tests (list (list " (())  "          ; contents
-                                 "((((((("          ; input
-                                 #'autopair-pair-p  ; predicate
-                                 "yyyyyyy"          ; expected
-                                 nil)               ; let-style-test-env
-                           (list " ((()) "
-                                 "((((((("
-                                 #'autopair-pair-p
-                                 "yyyyyyy")
-                           (list " (())) "
-                                 "((((((("
-                                 #'autopair-pair-p
-                                 "------y")
-                           (list "()   ) "
-                                 "---(---"
-                                 #'autopair-pair-p
-                                 "-------")
-                           (list " ((()) "
-                                 "----))-"
-                                 #'autopair-skip-p
-                                 "-------")
-                           (list " (())  "
-                                 "---))--"
-                                 #'autopair-skip-p
-                                 "---yy--")
-                           (list " (())) "
-                                 "---)))-"
-                                 #'autopair-skip-p
-                                 "---yyy-")
-                           ;; some mixed paren situations
-                           (list "  ()]  "
-                                 "-(-----"
-                                 #'autopair-pair-p
-                                 "-y-----")
-                           (list "  (])  "
-                                 "-(-----"
-                                 #'autopair-pair-p
-                                 "-y-----")
-                           (list "  ()]  "
-                                 "-[-----"
-                                 #'autopair-pair-p
-                                 "-------")
-                           (list "( ()]) "
-                                 "-[-----"
-                                 #'autopair-pair-p
-                                 "-------")
-                           (list "( ()]) "
-                                 "-(-----"
-                                 #'autopair-pair-p
-                                 "-y-----")
-                           (list " [([())  "
-                                 "-----))--"
-                                 #'autopair-skip-p
-                                 "-----y---")
-                           (list " [([])   "
-                                 "-----)---"
-                                 #'autopair-skip-p
-                                 "-----y---")))
+(define-autopair-simple-predicate-test balanced-situation
+  " (())  " "(((((((" autopair-pair-p "yyyyyyy")
 
-(defvar autopair-autowrap-tests)
-(setq autopair-autowrap-tests (list
-                               (list #'(lambda ()
-                                         (insert "hello") (set-mark (point)) (beginning-of-buffer))
-                                     "("
-                                     #'(lambda ()
-                                         (cons (buffer-substring-no-properties (point-min) (point-max))
-                                               (point)))
-                                     '("(hello)" . 2))
-                               (list #'(lambda ()
-                                         (insert "hello") (set-mark (point)) (beginning-of-buffer))
-                                     ")"
-                                     #'(lambda ()
-                                         (cons (buffer-substring-no-properties (point-min) (point-max))
-                                               (point)))
-                                     '("(hello)" . 8))
-                               (list #'(lambda ()
-                                         (insert "hello") (set-mark (point)) (beginning-of-buffer)
-                                         (exchange-point-and-mark))
-                                     ")"
-                                     #'(lambda ()
-                                         (cons (buffer-substring-no-properties (point-min) (point-max))
-                                               (point)))
-                                     '("(hello)" . 8))
-                               (list #'(lambda ()
-                                         (insert "hello") (set-mark (point)) (beginning-of-buffer)
-                                         (exchange-point-and-mark))
-                                     "("
-                                     #'(lambda ()
-                                         (cons (buffer-substring-no-properties (point-min) (point-max))
-                                               (point)))
-                                     '("(hello)" . 2))))
+(define-autopair-simple-predicate-test too-many-openings
+  " ((()) " "(((((((" autopair-pair-p "yyyyyyy")
 
-(dolist (p '(autopair-pair-p autopair-skip-p autopair-extra-skip-p autopair-extra-pair-p))
-  (put p 'autopair-test-charwise-predicate t))
+(define-autopair-simple-predicate-test too-many-closings
+  " (())) " "(((((((" autopair-pair-p "------y")
 
-(defun autopair-test (before
-                      input
-                      extractor-or-predicate)
-    (if (stringp before) (insert before))
-    (cond ((and (symbolp extractor-or-predicate)
-                (get extractor-or-predicate 'autopair-test-charwise-predicate))
-           (let* ((size (1- (point-max)))
-                  (result (make-string size ?-)))
-             (dotimes (i size)
-               (goto-char (1+ i))
-               (let ((autopair-inserted (aref input i)))
-                 (when (and (not (eq autopair-inserted ?-))
-                            (funcall extractor-or-predicate) (aset result i ?y)))))
-             result))
-          (t
-           (funcall before)
-           (if (functionp input)
-               (funcall input)
-             (let ((last-command-event (aref input 0)))
-               (call-interactively (key-binding input) nil)
-               (autopair-post-command-handler)))
-           (funcall extractor-or-predicate))))
+(define-autopair-simple-predicate-test too-many-closings-2
+  "()   ) " "---(---" autopair-pair-p "-------")
 
-(defun autopair-run-tests (&optional suite)
-  (interactive)
-  (let ((passed 0)
-        (failed 0))
-    (with-output-to-temp-buffer "*autopair-tests*"
-      (dolist (spec (or suite (append autopair-tests
-                                      autopair-extra-tests
-                                      autopair-autowrap-tests)))
-        (let* ((fspec (fourth spec))
-               (expected (or (and (functionp fspec) (funcall fspec))
-                             fspec
-                             t))
-               (actual (condition-case e
-                           (with-current-buffer (generate-new-buffer "*autopair-test*")
-                             (emacs-lisp-mode)
-                             (autopair-mode 1)
-                             (transient-mark-mode 1)
-                             (setq autopair-extra-pairs nil
-                                   autopair-dont-pair nil
-                                   autopair-handle-action-fns nil
-                                   autopair-handle-wrap-action-fns nil)
-                             (eval `(let ,(fifth spec)
-                                      (autopair-test (first spec)
-                                                     (second spec)
-                                                     (third spec)))))
-                         (error (error e)))))
-          (condition-case err
-              (progn (assert (equal actual expected)
-                             'show-args
-                             (format "test \"%s\" for input %s returned %s instead of %s\n"
-                                     (first spec)
-                                     (second spec)
-                                     actual
-                                     expected))
-                     (incf passed))
-            (error (progn
-                     (princ (cadr err))
-                     (incf failed))))))
-      (princ (format "%s%s tests total, %s pass, %s failures"
-                     (or (and (zerop failed) "") "\n\n")
-                     (+ passed failed)
-                     passed
-                     failed)))
-    (when noninteractive
-      (with-current-buffer "*autopair-tests*"
-        (princ (buffer-substring-no-properties (point-min) (point-max))))
-      (kill-emacs failed))))
+(define-autopair-simple-predicate-test balanced-autoskipping
+  " (())  " "---))--" autopair-skip-p "---yy--")
 
+(define-autopair-simple-predicate-test too-many-openings-autoskipping
+  " ((()) " "----))-" autopair-skip-p "-------")
+
+(define-autopair-simple-predicate-test too-many-closings-autoskipping
+  " (())) " "---)))-" autopair-skip-p "---yyy-")
+
+(define-autopair-simple-predicate-test mixed-paren-1
+  "  ()]  " "-(-----" autopair-pair-p "-y-----")
+
+(define-autopair-simple-predicate-test mixed-paren-2
+  "  (])  " "-(-----" autopair-pair-p "-y-----")
+
+(define-autopair-simple-predicate-test find-matching-different-paren-type
+  "  ()]  " "-[-----" autopair-pair-p "-------")
+
+(define-autopair-simple-predicate-test find-matching-different-paren-type-inside-list
+  "( ()]) " "-[-----" autopair-pair-p "-------")
+
+(define-autopair-simple-predicate-test ignore-different-unmatching-paren-type
+  "( ()]) " "-(-----" autopair-pair-p "-y-----")
+
+(define-autopair-simple-predicate-test skip-over-partially-balanced
+  " [([])   " "-----)---" autopair-skip-p "-----y---")
+
+(define-autopair-simple-predicate-test only-skip-over-at-least-partially-balanced-stuff
+  " [([())  " "-----))--" autopair-skip-p "-----y---")
+
+;; extra pairs tests
+;;
+(define-autopair-simple-predicate-test pair-of-backtick-and-quote
+  "       " "-----`-" autopair-extra-pair-p "-----y-"
+  ((autopair-extra-pairs '(:everywhere ((?` . ?'))))))
+
+(define-autopair-simple-predicate-test pair-backtick-and-quote-but-only-inside-string
+  "\"     \"  " "-----`--`" autopair-extra-pair-p "-----y---"
+  ((autopair-extra-pairs '(:string     ((?` . ?'))))))
+
+(define-autopair-simple-predicate-test skip-backtick-and-quote
+  "   ` ' " "-----'-" autopair-extra-skip-p "-----y-"
+  ((autopair-extra-pairs '(:everywhere ((?` . ?'))))))
+
+(define-autopair-simple-predicate-test skip-backtick-and-quote-but-only-inside-string
+  "  \"   \"" "-`---`-" autopair-extra-pair-p "-----y-"
+  ((autopair-extra-pairs '(:string     ((?` . ?'))))))
+
+;; autowrap tests
+;;
+(define-autopair-functional-test autowrap-from-beginning
+  #'(lambda ()
+      (insert "hello") (set-mark (point)) (beginning-of-buffer))
+  "(" "(hello)"  2)
+(define-autopair-functional-test autowrap-to-end
+  #'(lambda ()
+          (insert "hello") (set-mark (point)) (beginning-of-buffer))
+  ")" "(hello)"  8)
+(define-autopair-functional-test autowrap-from-end-stay-at-end
+  #'(lambda ()
+          (insert "hello") (set-mark (point)) (beginning-of-buffer)
+          (exchange-point-and-mark))
+  ")" "(hello)"  8)
+(define-autopair-functional-test autowrap-from-end-go-to-beginning
+  #'(lambda ()
+          (insert "hello") (set-mark (point)) (beginning-of-buffer)
+          (exchange-point-and-mark))
+  "(" "(hello)"  2)
 
 (provide 'autopair-tests)
 ;;; autopair-tests.el ends here
